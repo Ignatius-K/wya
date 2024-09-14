1ddeff700d9136b09e806c2c5a4468bb7915a911
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-09-14 16:48:04 +0300

    update

--- web/core/views.py
+++ web/core/views.py
@@ -3,14 +3,18 @@ This module defines the routes for the core module.
 """
 
 import random
-from flask import abort, make_response, render_template, send_from_directory, url_for, current_app
+from flask import abort, make_response, render_template, send_from_directory, url_for, current_app, request
 from uuid import uuid4
 
 from web.core import core_bp
 from models import storage
 from models.event import Event
 
-# define favicon route
+
+def get_site_image_url():
+    """Get full URL for site"""
+    return request.url_root + url_for('static', filename='images/happy_people.png')
+
 def get_favicorn():
     if current_app.static_folder is not None:
         return send_from_directory(
@@ -18,7 +22,6 @@ def get_favicorn():
             path='images/base_logo.png'
         )
     abort(404)
-
 
 core_bp.add_url_rule(
     "/favicon.ico", view_func=get_favicorn)
